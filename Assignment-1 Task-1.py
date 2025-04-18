fabf58f4babe859b992fd02fec3a870452dd6cd0
Gitzubi <zubi.study@gmail.com>
2025-04-18 12:00:06 +0530

    Add files via upload

---
+++ Assignment-1 Task-1.py
@@ -0,0 +1,12 @@
+num_1= input("Enter First Number: ")
+num_2= input("Enter second number: ")
+num_1= int(num_1)
+num_2= int(num_2)
+addition= num_1 + num_2
+subtraction= num_1 - num_2
+multiplication= num_1 * num_2
+division= num_1 / num_2
+print("addition: ", addition)
+print("subtraction: ", subtraction)
+print("multiplication: ", multiplication)
+print("division: ", division)
---
+++ Assignment-1 Task-2.py
@@ -0,0 +1,4 @@
+first_name= input("Enter your First Name: ")
+last_name= input("Enter your Last Name: ")
+full_name= first_name + " " + last_name
+print("Hello, " + full_name + "!" + " Welcome to the Python Program")
